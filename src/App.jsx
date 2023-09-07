27755aac6241adfd2cc9c86c5937de11176a1e25
chucoyos <unodev@outlook.com>
2023-09-07 00:38:47 -0600

    Install MUI and react router

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import './App.css'
+import Button from '@mui/material/Button'
+import Container from '@mui/material/Container'
+import Typography from '@mui/material/Typography'
+
+function App() {
+	return (
+		<>
+			<Container>
+				<h1>Shopping Cart</h1>
+				<Button variant='contained'>Hello World</Button>
+				<Typography variant='body1'>Hello World</Typography>
+			</Container>
+		</>
+	)
+}
+
+export default App
