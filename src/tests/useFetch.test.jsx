07c74f072737c1cf6dee83398bdc756a147443a7
chucoyos <unodev@outlook.com>
2023-09-29 03:21:08 -0600

    Add first test version for useFetch

--- src/tests/useFetch.test.jsx
+++ src/tests/useFetch.test.jsx
@@ -1,41 +1,30 @@
-import { describe, it, expect, vi, afterEach } from 'vitest'
+import { describe, it, expect, vi } from 'vitest'
 import useFetch from '../useFetch'
+import axios from 'axios'
 
 describe('useFetch', () => {
-	vi.mock('react-router-dom', async () => {
-		const actual = await vi.importActual('react-router-dom')
+	axios.get = vi.fn()
+	vi.mock('axios', async () => {
+		const actual = await vi.importActual('axios')
 		return {
 			...actual,
-			useOutletContext: () => ({
-				url: 'https://fakestoreapi.com/products/',
-			}),
+			get: vi.fn(),
 		}
-	})
-	afterEach(() => {
-		vi.resetAllMocks()
 	})
 
 	it('fetches data from the API', async () => {
-		const mockData = [
+		const products = [
 			{ id: 1, name: 'Product 1' },
 			{ id: 2, name: 'Product 2' },
 		]
-		vi.mock('../useFetch', async () => {
-			const actual = await vi.importActual('../useFetch')
-			return {
-				...actual,
-				useFetch: () => ({
-					data: mockData,
-					loading: false,
-					error: null,
-				}),
-			}
-		})
+		// vi.mock('axios')
+		axios.get.mockImplementationOnce(() => Promise.resolve({ data: products }))
+		// axios.get.mockResolvedValue({ data: products })
 		const { data, loading, error } = useFetch(
 			'https://fakestoreapi.com/products'
 		)
-		expect(useFetch).toHaveBeenCalledWith('https://fakestoreapi.com/products')
-		expect(data).toEqual(mockData)
+		expect(axios.get).toHaveBeenCalledWith('https://fakestoreapi.com/products')
+		expect(data).toEqual(products)
 		expect(loading).toBe(false)
 		expect(error).toBe(null)
 	})
@@ -43,6 +32,7 @@ describe('useFetch', () => {
 	// it('handles errors when fetching data', async () => {
 	// 	const mockError = 'An error occurred'
 	// 	axios.get.mockRejectedValueOnce(new Error(mockError))
+	// 	axios.get.mockImplementationOnce(() => Promise.reject(mockError))
 	// 	const { data, loading, error } = useFetch(
 	// 		'https://fakestoreapi.com/products'
 	// 	)
